10839d7f36924b81e5692e2626cbc2578b14740d
Kaustav Roy <54073521+kaustavr19@users.noreply.github.com>
2021-03-08 21:13:21 +0530

    rows in businesses where the founding year was before 1000

--- Task1.sql
+++ Task1.sql
@@ -1,3 +1,8 @@
 /*Select the oldest and newest founding years from the businesses table*/
 SELECT MIN(year_founded) AS Oldest, MAX(year_founded) AS Newest
 FROM businesses;
+
+/*Get the count of rows in businesses where the founding year was before 1000*/
+SELECT COUNT(*)
+FROM businesses
+WHERE year_founded < 1000;
